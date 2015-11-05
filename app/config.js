69eefcbc168318386b11bbf5d4fecd5b34c597ba
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-11-04 19:04:08 -0800

    added environment condition

--- app/config.js
+++ app/config.js
@@ -1,17 +1,31 @@
 var Bookshelf = require('bookshelf');
 var path = require('path');
+var db;
 
-var db = Bookshelf.initialize({
-  client: 'sqlite3',
-  connection: {
-    host: '127.0.0.1',
-    user: 'your_database_user',
-    password: 'password',
-    database: 'shortlydb',
-    charset: 'utf8',
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  }
-});
+if ( process.env.NODE_ENV='production') {
+  db = Bookshelf.initialize({
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    searchPath: 'knex,public'
+  });
+} else {
+  // dev
+  db = Bookshelf.initialize({
+    client: 'sqlite3',
+    connection: {
+      host: '127.0.0.1',
+      user: 'your_database_user',
+      password: 'password',
+      database: 'shortlydb',
+      charset: 'utf8',
+      filename: path.join(__dirname, '../db/shortly.sqlite')
+    }
+  });
+
+}
+
+
+
 
 db.knex.schema.hasTable('urls').then(function(exists) {
   if (!exists) {
